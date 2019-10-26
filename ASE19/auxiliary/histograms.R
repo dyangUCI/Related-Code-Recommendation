6c336a69049c14c1c0a176bdea209a5545140f77
dyangUCI <happy.d.yang@gmail.com>
2019-10-25 22:19:43 -0700

    fixes of figures, figure names

--- ASE19/auxiliary/histograms.R
+++ ASE19/auxiliary/histograms.R
@@ -1,4 +1,4 @@
-gh_count = read.table("/Users/Di/Documents/Related-Code-Recommendation/numOfSimilarGH.txt")
+gh_count = read.table("/Users/Di/Documents/Related-Code-Recommendation/auxiliary/numOfSimilarGH.txt")
 summary(gh_count$V1)
 gh = gh_count[gh_count$V1 <= 50, ]
 hist(gh, 
@@ -13,12 +13,12 @@ hist(gh,
 axis(1,cex.axis=1.5)
 axis(2,cex.axis=1.5)
 
-related_count = read.table("/Users/Di/Documents/Related-Code-Recommendation/numOfRelated.txt")
+related_count = read.table("/Users/Di/Documents/Related-Code-Recommendation/ASE19/auxiliary/numOfRelated.txt")
 summary(related_count$V1)
 related = related_count[related_count$V1 <= 200, ]
 hist(related, 
      main="", 
-     xlab="Number of related methods", 
+     xlab="Number of methods", 
      ylab = "Number of SO snippets",
      xlim=c(0, 200),
      xaxt="n",
@@ -29,17 +29,17 @@ axis(1,cex.axis=1.5)
 axis(2,cex.axis=1.5)
 
 
-avg_loc_count = read.table("/Users/Di/Documents/Related-Code-Recommendation/avgLocOfRelated.txt")
+avg_loc_count = read.table("/Users/Di/Documents/Related-Code-Recommendation/ASE19/auxiliary/avgLocOfRelated.txt")
 summary(avg_loc_count$V1)
 hist(avg_loc_count$V1, 
      main="", 
-     xlab="Average LOC for related methods", 
+     xlab="Average LOC for methods", 
      ylab = "Number of SO snippets",
 )
 avg_loc = avg_loc_count[avg_loc_count$V1 <= 60, ]
 hist(avg_loc, 
      main="", 
-     xlab="Average LOC for related methods", 
+     xlab="Average LOC for methods", 
      ylab = "Number of SO snippets",
      xlim=c(0, 60),
      xaxt="n",
